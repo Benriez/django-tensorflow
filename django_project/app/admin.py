9bdc82098d5f67bbeefac5f46aec252b882730a7
Benriez <33022072+Benriez@users.noreply.github.com>
2022-03-06 11:50:04 +0100

    rm unused

--- django_project/app/admin.py
+++ django_project/app/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from .models import Customer, Product, Order, Termine, Signale, CRM, Kommunikationstool, Warenwirtschaftssystem
-
-from .views import CRMView, KommunikationstoolView, WarenwirtschaftssystemView
+from .models import Images
 
 
-@admin.register(Customer)
+@admin.register(Images)
 class CustomerModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'website', 'kontakt')
 
@@ -16,54 +14,4 @@ class CustomerModelAdmin(admin.ModelAdmin):
         except:
             contact = obj.name
             
-        return contact
-
-
-@admin.register(Product)
-class ProductModelAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'preis', 'comment',)
-
-
-@admin.register(Order)
-class OrderModelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'kunde', 'erstellt',)
-    pass
-
-
-@admin.register(Termine)
-class TerminModelAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'kunde', 'datum', 'comment',)
-
-
-@admin.register(Signale)
-class SignalModelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'kunde', 'erstellt',)    
-
-
-@admin.register(CRM)
-class CRMModelAdmin(admin.ModelAdmin):
-    def get_urls(self):
-        view_name = '{}_{}_changelist'.format(
-                CRM._meta.app_label, CRM._meta.model_name)
-        return [
-            path('', CRMView.as_view(), name=view_name)
-        ] 
-
-@admin.register(Kommunikationstool)
-class KommunikationstoolModelAdmin(admin.ModelAdmin):
-    def get_urls(self):
-        view_name = '{}_{}_changelist'.format(
-                Kommunikationstool._meta.app_label, Kommunikationstool._meta.model_name)
-        return [
-            path('', KommunikationstoolView.as_view(), name=view_name) 
-        ] 
-
-
-@admin.register(Warenwirtschaftssystem)
-class WarenwirtschaftssystemModelAdmin(admin.ModelAdmin):
-    def get_urls(self):
-        view_name = '{}_{}_changelist'.format(
-                Warenwirtschaftssystem._meta.app_label, Warenwirtschaftssystem._meta.model_name)
-        return [
-            path('', WarenwirtschaftssystemView.as_view(), name=view_name)
-        ] 
+        return contact 
