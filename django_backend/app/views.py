4c874fd08c60d71d03068db352d03e4097962454
Benriez <stberiez@stud.h-da.de>
2022-10-10 15:38:33 +0200

    update summary

--- django_backend/app/views.py
+++ django_backend/app/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import HttpResponse, HttpResponseRedirect
-from django.views.generic import TemplateView
 
-from .models import Images
 
 def index(request):
     if "next" in request.POST:
-        return render(request, 'summary.html')
+        return redirect("summary")
 
     return render(request, 'index.html')
 
   
- 
+def summary(request):
+    # validate user data exists before accepting else -> redirect index
+    return render(request, 'summary.html')
