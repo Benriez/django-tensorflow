471cd7ce176b7f945c6396b47bec15b9c9376ebe
Benriez <stberiez@stud.h-da.de>
2022-10-10 12:12:38 +0200

    rm unused

--- django_project/app/views.py
+++ django_project/app/views.py
@@ -5,18 +5,7 @@ from django.views.generic import TemplateView
 from .models import Images
 
 def index(request):
-    if request.user.is_superuser:
-        return render(request, 'server.html') 
-    
-    else:
-        return render(request, 'client.html')
+    return render(request, 'index.html')
 
         
  
-#------------------------------------------------------------------
-def start_slideshow(request):
-    img = Images.objects.all()
-    context = {
-        'img':img
-    }
-    return render(request, 'server.html', context)    
