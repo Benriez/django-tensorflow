4e1401eeab8f79975708adb41fdfaabc67f3d299
Benriez <stberiez@stud.h-da.de>
2022-10-10 15:31:54 +0200

    add summary view

--- django_backend/app/views.py
+++ django_backend/app/views.py
@@ -5,7 +5,10 @@ from django.views.generic import TemplateView
 from .models import Images
 
 def index(request):
+    if "next" in request.POST:
+        return render(request, 'summary.html')
+
     return render(request, 'index.html')
 
-        
+  
  
