31ed9a2d78a23a7ca4b78db46e720cb594476120
Benriez <stberiez@stud.h-da.de>
2022-10-12 18:26:46 +0200

    update forms

--- django_backend/app/views.py
+++ django_backend/app/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render, redirect
-from django.shortcuts import HttpResponse, HttpResponseRedirect
 
 
 def index(request):
     if "next" in request.POST:
+        anrede = request.POST.get("anrede")
+        vorname = request.POST.get("vorname")
+        nachname = request.POST.get("nachname")
+        email_addresse = request.POST.get("email-addresse")
+        geburtsdatum = request.POST.get("geburtsdatum")
+        strasse = request.POST.get("strasse")
+        plz = request.POST.get("plz")
+        ort = request.POST.get("ort")
+        iban = request.POST.get('iban')
+
+        request.session["anrede"] = anrede
+        request.session["vorname"] = vorname
+        request.session["nachname"] = nachname
+        request.session["strasse"] = strasse
+        request.session["plz"] = plz
+        request.session["ort"] = ort
+        request.session["email"] = email_addresse
+        request.session["geburtsdatum"] = geburtsdatum
+        request.session["iban"] = iban
+
+        # get BIC
+        # get bank name
         return redirect("summary")
 
     return render(request, 'index.html')
