40a9e951d0caad223bfc81f6a9d9cd99a82fdb79
Benriez <stberiez@stud.h-da.de>
2022-11-06 00:51:31 +0100

    fix

--- utils/run.sh
+++ utils/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python manage.py collectstatic --noinput && python manage.py migrate && gunicorn django_project.wsgi --bind=0.0.0.0:80
+python3 manage.py collectstatic --noinput && python3 manage.py migrate && gunicorn django_project.wsgi --bind=0.0.0.0:80
 
