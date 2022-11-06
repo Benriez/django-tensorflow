0f332e51db3a05a85473408b636f793a34f95b47
Benriez <stberiez@stud.h-da.de>
2022-11-06 03:41:03 +0100

    update asgi

--- django_backend/app/routing.py
+++ django_backend/app/routing.py
@@ -4,8 +4,6 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/scraper/$', consumers.ScraperViewConsumer.as_asgi()),
-    re_path(r'wss/scraper/$', consumers.ScraperViewConsumer.as_asgi()),
     re_path(r'ws/extra-scraper/$', consumers.ExtraViewConsumer.as_asgi()),
-    re_path(r'wss/extra-scraper/$', consumers.ExtraViewConsumer.as_asgi())
     #re_path(r'ws/client/$', consumers.ClientConsumer.as_asgi()),
 ]
